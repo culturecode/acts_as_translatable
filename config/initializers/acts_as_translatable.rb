fd356b95f4d5bc10d43ef84ab3d9d48fb696e5d9
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2011-12-03 17:02:47 -0800

    Fixed bug that caused fields with no value to not show a translatable input field.

--- config/initializers/acts_as_translatable.rb
+++ config/initializers/acts_as_translatable.rb
@@ -3,15 +3,16 @@ class SimpleForm::FormBuilder
   def translated_input(attribute_name, options = {})    
     options.reverse_merge! :wrapper_html => {}
     output = "".html_safe
-    if object.send(attribute_name).present?
-      template.content_tag :div, :class => "form_group" do
-        # Because the translated_attribute is a pseudo attribute, we need to find out what input type to make or simple form will just make a string input. 
-        input_type = default_input_type(attribute_name, find_attribute_column(attribute_name), {})
-        output = input(attribute_name, options)
-        options[:wrapper_html].merge!(:class => "translation_field")
-        options[:as] = input_type
-        output << input("#{attribute_name}_translated", options)
-      end
+
+    template.content_tag :div, :class => "form_group" do
+      # Because the translated_attribute is a pseudo attribute, we need to find out what input type to make or simple form will just make a string input. 
+      input_type = default_input_type(attribute_name, find_attribute_column(attribute_name), {})
+      output = input(attribute_name, options)
+      options[:wrapper_html].merge!(:class => "translation_field")
+      options[:as] = input_type
+      output << input("#{attribute_name}_translated", options)
     end
+
+    return output
   end
 end
